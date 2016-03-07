8141dc03c661a0bb7486cd2fee5c50631326fd65
Jarvist Moore Frost <jarvist@gmail.com>
2016-03-07 12:43:30 +0000

    Delete Gromacs (Again :^)

--- gromacs-4.6.5/build/share/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/er113/2014-05_beth_pcbm-cg-dos/gromacs-4.6.5/share
-# Build directory: /Users/er113/2014-05_beth_pcbm-cg-dos/gromacs-4.6.5/build/share
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(template)
--- gromacs-4.6.5/build/share/template/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/er113/2014-05_beth_pcbm-cg-dos/gromacs-4.6.5/share/template
-# Build directory: /Users/er113/2014-05_beth_pcbm-cg-dos/gromacs-4.6.5/build/share/template
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
